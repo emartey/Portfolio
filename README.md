8f47f890746b2e472f4170feef03abd917fdb642
emartey <54255043+emartey@users.noreply.github.com>
2021-03-18 22:33:55 -0500

    Updates

--- README.md
+++
@@ -1,3 +0,0 @@
-# Edward Martey Portfolio
-
-Welcome to my portfolio app! A brief showcase of my background and skills. Thanks for coming, if you see something you like or desire to connect feel free to reach out to me on LinkedIn:  https://www.linkedin.com/in/emartey/
---
+++ precache-manifest.6c2124196cae409be0d7a1d665cabe53.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9b5e155b4d4d176afa91",
+    "url": "/Portfolio/static/css/main.7491c672.chunk.css"
+  },
+  {
+    "revision": "9b5e155b4d4d176afa91",
+    "url": "/Portfolio/static/js/main.9b5e155b.chunk.js"
+  },
+  {
+    "revision": "dbd10dfce55af708dd99",
+    "url": "/Portfolio/static/js/1.dbd10dfc.chunk.js"
+  },
+  {
+    "revision": "3b96116072eb85f5a3a2",
+    "url": "/Portfolio/static/js/runtime~main.3b961160.js"
+  },
+  {
+    "revision": "a74bbe8fc8500b0f707423eb702bd103",
+    "url": "/Portfolio/index.html"
+  }
+];
